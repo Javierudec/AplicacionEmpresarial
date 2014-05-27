8baee05528205ef12030b8f69c001f7de422139f
Javier Gonzalez <javier.udec.91@gmail.com>
2014-05-26 20:02:35 -0400

    Implemented: Validation constraints to user form

--- Aplicacion/src/main/java/entities/AccountInformation.java
+++ Aplicacion/src/main/java/entities/AccountInformation.java
@@ -1,7 +1,14 @@
 package entities;
 
+import org.apache.tapestry5.beaneditor.Validate;
+
 public class AccountInformation {
+	@Validate("required")
 	public String username;
+	
+	@Validate("required,minLength=6,maxLength=20")
 	public String password;
+	
+	@Validate("required,regexp=^[_a-z0-9-]+(\\.[_a-z0-9-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*(\\.[a-z]{2,4})$")
 	public String email;
 }
